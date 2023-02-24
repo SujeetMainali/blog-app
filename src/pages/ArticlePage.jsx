24de49849b19e4195859ceebb75ab5faa6ef2fbb
Sujeet Mainali <mainalisujeet@gmail.com>
2023-02-25 03:49:16 +0545

    added upvote button and functionality to the button

--- src/pages/ArticlePage.jsx
+++ src/pages/ArticlePage.jsx
@@ -11,33 +11,43 @@ const ArticlePage = () => {
     const [articleInfo, setArticleInfo] = useState({ upvotes: 0, comments: [] });
     const { articleId } = useParams();
     useEffect(() => {
-        const loadArticleInfo = async()=>{
+        const loadArticleInfo = async () => {
             const response = await axios.get(`/api/articles/${articleId}`);
             const newArticleInfo = response.data;
             setArticleInfo(newArticleInfo)
         }
         loadArticleInfo();
         // setArticleInfo({ upvotes : 3 , comments: [] });
-        },[]);
-   
+    }, []);
+
     //all three ways are correct ways to get id in url parameters.
     const article = articles.find(article => article.name === articleId);
 
-    if(!article){
-        return (<NotFoundPage/>)
+    const addUpvote = async () => {
+      let  response = await axios.put(`/api/articles/${articleId}/upvote`);
+        const updatedArticle = response.data;
+        setArticleInfo(updatedArticle);
+    }
+
+    if (!article) {
+        return (<NotFoundPage />)
     }
     return (
         <>
             <h1>
                 {article.name}
             </h1>
-            <p>this article has {articleInfo.upvotes} upvotes </p>
-                {article.content.map((paragraph, i) =>(
-                    <p key={i}>{paragraph}</p>
-                ))}
-                <CommentList comments={ articleInfo.comments} />
-            
-            </>
+            <div className="upvotes-section">
+                <button onClick={addUpvote}>Upvote</button>
+                <p>this article has {articleInfo.upvotes} upvotes </p>
+            </div>
+
+            {article.content.map((paragraph, i) => (
+                <p key={i}>{paragraph}</p>
+            ))}
+            <CommentList comments={articleInfo.comments} />
+
+        </>
     )
 }
 
