9697211719c3937732d24c2e90399228ed06cfb6
Sujeet Mainali <mainalisujeet@gmail.com>
2023-02-27 22:01:55 +0545

    changes in login page

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,8 +1,20 @@
+import { useState } from "react";
+import { Link } from "react-router-dom";
+
 const LoginPage = ()=>{
+    const [email, setEmail] = useState('');
+    const [ password, setPassword ] = useState('');
+    const [error, setError ] = useState('');
     return(
+        <>
         <h1>
             Login Page 
         </h1>
+        <input type="text" />
+        <input type="password" />
+        <button>Log In</button>
+        <Link to="/create-account">Create account</Link>
+        </>
     )
 }
 
