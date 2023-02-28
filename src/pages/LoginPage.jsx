1b9cf3132e57552037fa80d665cff98530fc1c4f
Sujeet Mainali <mainalisujeet@gmail.com>
2023-02-28 23:16:51 +0545

    small changes

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,18 +1,39 @@
 import { useState } from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import { getAuth, signInWithEmailAndPassword} from 'firebase/auth'
 
 const LoginPage = ()=>{
     const [email, setEmail] = useState('');
     const [ password, setPassword ] = useState('');
     const [error, setError ] = useState('');
+    const navigate = useNavigate()
+
+    const logIn =async()=>{
+        try {
+            await signInWithEmailAndPassword(getAuth(),email,password);
+            navigate('/articles');
+        } catch (e) {
+            setError(e.message)
+        }
+    }
+
     return(
         <>
         <h1>
             Login Page 
         </h1>
-        <input type="text" />
-        <input type="password" />
-        <button>Log In</button>
+        {error && <p className="error ">{error}</p>}
+        <input type="text" 
+        placeholder="Email"
+        value={email}
+        onChange={e=>setEmail(e.target.value)}
+        />
+        <input type="password" 
+        placeholder="Password"
+        value={password}
+        onChange={e=>setPassword(e.target.value)}
+        />
+        <button onClick={logIn}>Log In</button>
         <Link to="/create-account">Create account</Link>
         </>
     )
